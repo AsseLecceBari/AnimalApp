ca19f3e9a9630d2043838d9bc47bdb8b62a2afda
Domenico <d.roberto8@studenti.uniba.it>
2022-11-10 21:04:39 +0100

    implementazione pulsante profilo

--- app/src/main/java/it/uniba/dib/sms2223_2/MainActivity.java
+++ app/src/main/java/it/uniba/dib/sms2223_2/MainActivity.java
@@ -3,12 +3,16 @@ package it.uniba.dib.sms2223_2;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TableLayout;
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
 
 import adapter.VPAdapter;
 
@@ -17,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager2 viewPager2;
     VPAdapter vpAdapter;
     private Toolbar main_action_bar;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,6 +86,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
+
+
         main_action_bar=findViewById(R.id.main_action_bar);
         setSupportActionBar(main_action_bar);
 
@@ -94,6 +101,27 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
        getMenuInflater().inflate(R.menu.menu_bar_main, menu);
        return true;
+
+
+
+    }
+
+    public void mostraProfilo(MenuItem item) {
+
+        auth= FirebaseAuth.getInstance();
+
+
+        if(auth.getCurrentUser()!=null){
+                
+
+
+        }
+        else{
+            startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+        }
+
+
+
 
 
 
