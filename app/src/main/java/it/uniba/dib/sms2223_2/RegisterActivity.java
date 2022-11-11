9901025b045cc628878731a7a077ee6e276d5a59
MontinaroEnrico <e.montinaro3@studenti.uniba.it>
2022-11-11 21:16:40 +0100

    Aggiunti controlli per visualizzare le caselle di testo nel form registrazione in base all'utente che si sta registrando. Aggiunto anche ritorno all'activity main quando ci si registra

--- app/src/main/java/it/uniba/dib/sms2223_2/RegisterActivity.java
+++ app/src/main/java/it/uniba/dib/sms2223_2/RegisterActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -14,6 +16,7 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -24,6 +27,12 @@ import java.util.Map;
 import model.Utente;
 
 public class RegisterActivity extends AppCompatActivity {
+    TextInputLayout numEFNInputLayout;
+    TextInputLayout piVAInputLayout;
+    TextInputLayout denomInputLayout;
+    TextInputLayout isPrivatoInputLayout;
+    TextInputLayout codFiscAssInputLayout;
+
 
     TextInputEditText etRegEmail;
     TextInputEditText etRegPassword;
@@ -53,6 +62,47 @@ public class RegisterActivity extends AppCompatActivity {
         etRegTelefono = findViewById(R.id.etRegTelefono);
         etRegIndirizzo = findViewById(R.id.etRegIndirizzo);
         etRegRuolo=findViewById(R.id.etRegRuolo);
+
+        numEFNInputLayout=findViewById(R.id.numEFNInputLayout);
+        piVAInputLayout=findViewById(R.id.piVAInputLayout);
+        denomInputLayout=findViewById(R.id.denomInputLayout);
+        isPrivatoInputLayout=findViewById(R.id.isPrivatoInputLayout);
+        codFiscAssInputLayout=findViewById(R.id.codFiscAssInputLayout);
+        etRegRuolo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i==0){
+                    numEFNInputLayout.setVisibility(View.GONE);
+                    piVAInputLayout.setVisibility(View.GONE);
+                    denomInputLayout.setVisibility(View.GONE);
+                    isPrivatoInputLayout.setVisibility(View.GONE);
+                    codFiscAssInputLayout.setVisibility(View.GONE);
+                }else if(i==1){
+                    numEFNInputLayout.setVisibility(View.VISIBLE);
+                    piVAInputLayout.setVisibility(View.VISIBLE);
+                    denomInputLayout.setVisibility(View.GONE);
+                    isPrivatoInputLayout.setVisibility(View.GONE);
+                    codFiscAssInputLayout.setVisibility(View.GONE);
+                }else if(i==2){
+                    numEFNInputLayout.setVisibility(View.GONE);
+                    piVAInputLayout.setVisibility(View.GONE);
+                    denomInputLayout.setVisibility(View.VISIBLE);
+                    isPrivatoInputLayout.setVisibility(View.GONE);
+                    codFiscAssInputLayout.setVisibility(View.VISIBLE);
+                }else if(i==3){
+                    numEFNInputLayout.setVisibility(View.GONE);
+                    piVAInputLayout.setVisibility(View.VISIBLE);
+                    denomInputLayout.setVisibility(View.VISIBLE);
+                    isPrivatoInputLayout.setVisibility(View.VISIBLE );
+                    codFiscAssInputLayout.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
         etRegNumEFNOVI = findViewById(R.id.etRegNumEFNOVI);
         etRegPartitaIva = findViewById(R.id.etRegPartitaIva);
         etRegDenominazione=findViewById(R.id.etRegDenominazione);
@@ -98,6 +148,7 @@ public class RegisterActivity extends AppCompatActivity {
             etRegConfPass.requestFocus();
         }else{
            create(email,password);
+
         }
     }
     private void  create(String email,String password){
@@ -112,6 +163,7 @@ public class RegisterActivity extends AppCompatActivity {
                     Toast.makeText(RegisterActivity.this, "Utente registrato", Toast.LENGTH_SHORT).show();
                     Utente u =new Utente(email+"",telefono+"",indirizzo,ruolo);
                     db.collection("utenti").document(email+"").set(u);
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
                 }else{
                     Toast.makeText(RegisterActivity.this, "Errore di registrazione: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                 }
@@ -119,5 +171,6 @@ public class RegisterActivity extends AppCompatActivity {
         });
 
     }
+
 
 }
