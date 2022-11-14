5631a26edbaedf836f9d1c46428112ff6deee3d5
martinopagano <104270272+martinocosimopagano@users.noreply.github.com>
2022-11-14 11:46:25 +0100

    completamento della registrazione con le informazioni nome cognome e data di nascita per propietario e veterinario nel form.

--- app/src/main/java/it/uniba/dib/sms2223_2/RegisterActivity.java
+++ app/src/main/java/it/uniba/dib/sms2223_2/RegisterActivity.java
@@ -3,12 +3,16 @@ package it.uniba.dib.sms2223_2;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.text.TextUtils;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,32 +25,43 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
 import model.Utente;
 
 public class RegisterActivity extends AppCompatActivity {
-    TextInputLayout numEFNInputLayout;
-    TextInputLayout piVAInputLayout;
-    TextInputLayout denomInputLayout;
-    TextInputLayout isPrivatoInputLayout;
-    TextInputLayout codFiscAssInputLayout;
+    private TextInputLayout numEFNInputLayout;
+    private TextInputLayout piVAInputLayout;
+    private TextInputLayout denomInputLayout;
+    private TextInputLayout isPrivatoInputLayout;
+    private TextInputLayout codFiscAssInputLayout;
+    private TextInputLayout dataLayout;
+    private TextInputLayout nomeLayout;
+    private TextInputLayout cognomeLayout;
+
+    private TextInputEditText etRegEmail;
+    private TextInputEditText etRegPassword;
+    private TextInputEditText etRegConfPass;
+    private TextInputEditText etRegTelefono;
+    private TextInputEditText etRegIndirizzo;
+    private Spinner etRegRuolo;
+    private TextInputEditText etRegNumEFNOVI;
+    private TextInputEditText etRegPartitaIva;
+    private TextInputEditText etRegDenominazione;
+    private TextInputEditText etRegIsPrivato;
+    private TextInputEditText etRegCodiceFiscaleAssociazione;
+    private TextInputEditText data;
+    private TextInputEditText nome;
+    private TextInputEditText cognome;
+
+    private TextView tvLoginHere;
+    private Button btnRegister;
 
 
-    TextInputEditText etRegEmail;
-    TextInputEditText etRegPassword;
-    TextInputEditText etRegConfPass;
-    TextInputEditText etRegTelefono;
-    TextInputEditText etRegIndirizzo;
-    Spinner etRegRuolo;
-    TextInputEditText etRegNumEFNOVI;
-    TextInputEditText etRegPartitaIva;
-    TextInputEditText etRegDenominazione;
-    TextInputEditText etRegIsPrivato;
-    TextInputEditText etRegCodiceFiscaleAssociazione;
-    TextView tvLoginHere;
-    Button btnRegister;
+    private DatePickerDialog picker;
+    private Calendar cldr;
 
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
@@ -56,59 +71,104 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        cldr = Calendar.getInstance();
+
+        // Informazioni generali
         etRegEmail = findViewById(R.id.etRegEmail);
         etRegPassword = findViewById(R.id.etRegPass);
-        etRegConfPass=findViewById(R.id.etRegConfPass);
+        etRegConfPass = findViewById(R.id.etRegConfPass);
         etRegTelefono = findViewById(R.id.etRegTelefono);
         etRegIndirizzo = findViewById(R.id.etRegIndirizzo);
-        etRegRuolo=findViewById(R.id.etRegRuolo);
+        etRegRuolo = findViewById(R.id.etRegRuolo);
+        nome = findViewById(R.id.nome);
+        cognome = findViewById(R.id.cognome);
+        data = findViewById(R.id.data);
 
-        numEFNInputLayout=findViewById(R.id.numEFNInputLayout);
-        piVAInputLayout=findViewById(R.id.piVAInputLayout);
-        denomInputLayout=findViewById(R.id.denomInputLayout);
-        isPrivatoInputLayout=findViewById(R.id.isPrivatoInputLayout);
-        codFiscAssInputLayout=findViewById(R.id.codFiscAssInputLayout);
+        // Informazioni specifiche
+        numEFNInputLayout = findViewById(R.id.numEFNInputLayout);
+        piVAInputLayout = findViewById(R.id.piVAInputLayout);
+        denomInputLayout = findViewById(R.id.denomInputLayout);
+        isPrivatoInputLayout = findViewById(R.id.isPrivatoInputLayout);
+        codFiscAssInputLayout = findViewById(R.id.codFiscAssInputLayout);
+        nomeLayout = findViewById(R.id.nomeLayout);
+        cognomeLayout = findViewById(R.id.cognomeLayout);
+        dataLayout = findViewById(R.id.dataLayout);
+
+        // Al click nel campo Nascita si apre il date picker
+        data.setInputType(InputType.TYPE_NULL);
+        data.setFocusable(false);
+        data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(RegisterActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                data.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
+
+        // Rendo visibili alcuni campi in base al ruolo
         etRegRuolo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                if (i==0){
-                    numEFNInputLayout.setVisibility(View.GONE);
-                    piVAInputLayout.setVisibility(View.GONE);
-                    denomInputLayout.setVisibility(View.GONE);
-                    isPrivatoInputLayout.setVisibility(View.GONE);
-                    codFiscAssInputLayout.setVisibility(View.GONE);
-                }else if(i==1){
-                    numEFNInputLayout.setVisibility(View.VISIBLE);
-                    piVAInputLayout.setVisibility(View.VISIBLE);
-                    denomInputLayout.setVisibility(View.GONE);
-                    isPrivatoInputLayout.setVisibility(View.GONE);
-                    codFiscAssInputLayout.setVisibility(View.GONE);
-                }else if(i==2){
-                    numEFNInputLayout.setVisibility(View.GONE);
-                    piVAInputLayout.setVisibility(View.GONE);
-                    denomInputLayout.setVisibility(View.VISIBLE);
-                    isPrivatoInputLayout.setVisibility(View.GONE);
-                    codFiscAssInputLayout.setVisibility(View.VISIBLE);
-                }else if(i==3){
-                    numEFNInputLayout.setVisibility(View.GONE);
-                    piVAInputLayout.setVisibility(View.VISIBLE);
-                    denomInputLayout.setVisibility(View.VISIBLE);
-                    isPrivatoInputLayout.setVisibility(View.VISIBLE );
-                    codFiscAssInputLayout.setVisibility(View.GONE);
+                switch (i){
+                    case 0: // propietario
+                        numEFNInputLayout.setVisibility(View.GONE);
+                        piVAInputLayout.setVisibility(View.GONE);
+                        denomInputLayout.setVisibility(View.GONE);
+                        isPrivatoInputLayout.setVisibility(View.GONE);
+                        codFiscAssInputLayout.setVisibility(View.GONE);
+                        mostraNomeCognomeNascita();
+                        break;
+
+                    case 1: // veterinaio
+                        numEFNInputLayout.setVisibility(View.VISIBLE);
+                        piVAInputLayout.setVisibility(View.VISIBLE);
+                        denomInputLayout.setVisibility(View.GONE);
+                        isPrivatoInputLayout.setVisibility(View.GONE);
+                        codFiscAssInputLayout.setVisibility(View.GONE);
+                        mostraNomeCognomeNascita();
+                        break;
+
+                    case 2: // associazione
+                        numEFNInputLayout.setVisibility(View.GONE);
+                        piVAInputLayout.setVisibility(View.GONE);
+                        denomInputLayout.setVisibility(View.VISIBLE);
+                        isPrivatoInputLayout.setVisibility(View.GONE);
+                        codFiscAssInputLayout.setVisibility(View.VISIBLE);
+                        nascondiNomeCognomeNascita();
+                        break;
+
+                    case 3: // ente
+                        numEFNInputLayout.setVisibility(View.GONE);
+                        piVAInputLayout.setVisibility(View.VISIBLE);
+                        denomInputLayout.setVisibility(View.VISIBLE);
+                        isPrivatoInputLayout.setVisibility(View.VISIBLE );
+                        codFiscAssInputLayout.setVisibility(View.GONE);
+                        nascondiNomeCognomeNascita();
+                        break;
                 }
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
-
+                Toast.makeText(RegisterActivity.this, R.string.selectUserType, Toast.LENGTH_SHORT).show();
             }
         });
+
         etRegNumEFNOVI = findViewById(R.id.etRegNumEFNOVI);
         etRegPartitaIva = findViewById(R.id.etRegPartitaIva);
         etRegDenominazione=findViewById(R.id.etRegDenominazione);
         etRegIsPrivato = findViewById(R.id.etRegIsPrivato);
         etRegCodiceFiscaleAssociazione = findViewById(R.id.etRegCodiceFiscaleAssociazione);
-
         tvLoginHere = findViewById(R.id.tvLoginHere);
         btnRegister = findViewById(R.id.btnRegister);
 
@@ -122,6 +182,18 @@ public class RegisterActivity extends AppCompatActivity {
         tvLoginHere.setOnClickListener(view ->{
             startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
         });
+    }
+
+    private void mostraNomeCognomeNascita() {
+        nomeLayout.setVisibility(View.VISIBLE);
+        cognomeLayout.setVisibility(View.VISIBLE);
+        dataLayout.setVisibility(View.VISIBLE);
+    }
+
+    private void nascondiNomeCognomeNascita() {
+        nomeLayout.setVisibility(View.GONE);
+        cognomeLayout.setVisibility(View.GONE);
+        dataLayout.setVisibility(View.GONE);
     }
 
     private void createUser(){
