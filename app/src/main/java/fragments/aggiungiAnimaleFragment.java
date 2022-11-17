95e0d634eda8f998b967fe31d880650ef2adb90a
MontinaroEnrico <e.montinaro3@studenti.uniba.it>
2022-11-17 11:19:15 +0100

    Miglioramenti nella funziuonalità aggiungi animale

--- app/src/main/java/fragments/aggiungiAnimaleFragment.java
+++ app/src/main/java/fragments/aggiungiAnimaleFragment.java
@@ -6,7 +6,6 @@ import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -14,7 +13,6 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.text.InputType;
@@ -25,7 +23,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -34,28 +31,19 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.local.BundleCache;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnPausedListener;
-import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
 import com.google.firebase.storage.UploadTask;
 
-import java.io.File;
-import java.io.Serializable;
-import java.net.HttpURLConnection;
 import java.net.InetAddress;
-import java.net.URL;
 import java.util.Calendar;
 import java.util.Random;
 
 import it.uniba.dib.sms2223_2.MainActivity;
 import it.uniba.dib.sms2223_2.R;
-import it.uniba.dib.sms2223_2.RegisterActivity;
-import it.uniba.dib.sms2223_2.ServiceUploadImage;
 import model.Animale;
-import model.Persona;
 
 public class aggiungiAnimaleFragment extends Fragment {
     private DatePickerDialog picker;
@@ -136,6 +124,7 @@ public class aggiungiAnimaleFragment extends Fragment {
         registraAnimaleBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 String nome= etRegNomeAnimale.getText().toString();
                 String genere=etRegGenereAnimale.getText().toString();
                 String specie=etRegSpecieAnimale.getText().toString();
@@ -148,17 +137,20 @@ public class aggiungiAnimaleFragment extends Fragment {
 
 
                 // si va sotto solo se si superano i controlli dell'input
-                Intent intent = new Intent(rootView.getContext(), ServiceUploadImage.class).putExtra("file",file.toString());
-                getActivity().startService(intent);
+                registraAnimaleBtn.setVisibility(View.INVISIBLE);
+               // Intent intent = new Intent(rootView.getContext(), ServiceUploadImage.class).putExtra("file",file.toString());
+                //getActivity().startService(intent);
 
                 Animale a = new Animale(nome, genere, specie, emailProprietario, dataDiNascita, fotoProfilo, idAnimale, isAssistito);
-                db.collection("animali").document(idAnimale).set(a).addOnSuccessListener(new OnSuccessListener<Void>() {
+                db.collection("animali").document(idAnimale).set(a).addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
-                    public void onSuccess(Void unused) {
+                    public void onComplete(@NonNull Task<Void> task) {
                     }
                 });
+                uploadImage();
                    // startActivity(new Intent(rootView.getContext(), MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
                 }
+
 
         });
 
@@ -166,6 +158,27 @@ public class aggiungiAnimaleFragment extends Fragment {
         return rootView;
     }
 
+    public void uploadImage() {
+        storage= FirebaseStorage.getInstance();
+        storageRef=storage.getReference();
+        StorageMetadata metadata = new StorageMetadata.Builder().setContentType("image/jpeg").build();
+        try{
+            StorageTask<UploadTask.TaskSnapshot> storageTask=storageRef.child("images/"+file.getLastPathSegment()).putFile(file, metadata);
+            if(isInternetAvailable()) {
+                storageTask.addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        Log.e("storage", storageTask + "");
+                        startActivity(new Intent(getContext(), MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
+                    }
+                });
+            }else{
+                startActivity(new Intent(getContext(), MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
+            }
+
+        }catch (Exception e){
+        }
+    }
     private boolean isNetworkConnected() {
         ConnectivityManager cm = (ConnectivityManager) getActivity().getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
 
