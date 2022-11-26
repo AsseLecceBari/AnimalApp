eb4e09ac1d29614867a1faa1a010844e7080fb1b
martinopagano <104270272+martinocosimopagano@users.noreply.github.com>
2022-11-26 19:53:47 +0100

    Update ritrovamento.java
    alla pressione del pulsante mostra le segnalazioni, in futuro deve mostrare solo segnalazioni di smarrimento in quella specifica zona

--- app/src/main/java/fragments_segnalazioni/ritrovamento.java
+++ app/src/main/java/fragments_segnalazioni/ritrovamento.java
@@ -2,8 +2,11 @@ package fragments_segnalazioni;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,11 +14,29 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+import adapter.ReportAdapter;
 import it.uniba.dib.sms2223_2.R;
+import model.Segnalazione;
 
 public class ritrovamento extends Fragment {
     private TextView indirizzo;
     private Button gps, mostra;
+
+    private FirebaseAuth auth;
+    private FirebaseFirestore db;
+    protected RecyclerView mRecyclerView;
+    protected ReportAdapter mAdapter;
+    protected ArrayList<Segnalazione> mDataset= new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -43,10 +64,41 @@ public class ritrovamento extends Fragment {
 
     public void mostraRyclerView() {
         // TODO: mostrare solo gli smarrimenti vicini l'indirizzo _____
-        // attualmente mostra tutti gli smarrimenti
+
+        // !!!!!!!!!! attualmente mostra tutti le segnalazioni di smarrimenti !!!!!!!!!!!!!!!!
         Toast.makeText(getActivity().getApplicationContext(), "Mostro in base ai tuoi filtri", Toast.LENGTH_LONG).show();
 
+        initDataset();
 
+    }
+
+    private void initDataset() {
+        mDataset.clear();
+        //Prendere gli oggetti(documenti)animali da fireBase e aggiungerli al dataset
+        db= FirebaseFirestore.getInstance();
+        auth= FirebaseAuth.getInstance();
+        CollectionReference segnalazioniRef=db.collection("segnalazioni");
+
+
+        if(auth.getCurrentUser()!=null){
+            segnalazioniRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if(task.isSuccessful()){
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            //Salvare animale in un array con elementi oggetto animale
+                            mDataset.add(document.toObject(Segnalazione.class));
+                            //Passo i dati presi dal database all'adapter
+                            mAdapter = new ReportAdapter(mDataset);
+                            // Setto l'AnimalAdaper(mAdapter) come l'adapter per la recycle view
+                            mRecyclerView.setAdapter(mAdapter);
+                        }
+                    }else {
+                        Log.d("ERROR", "Error getting documents: ", task.getException());
+                    }
+                }
+            });
+        }
     }
 
     public void aggiornaTextviewConCordinate() {
