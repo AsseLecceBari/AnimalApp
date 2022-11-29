c026810e7e7c68e5108677fe7abbca662ed10aed
MontinaroEnrico <e.montinaro3@studenti.uniba.it>
2022-11-29 19:12:33 +0100

    Aggiunta mappa in vista segnalazione

--- app/src/main/java/fragments_segnalazioni/vistaSegnalazione.java
+++ app/src/main/java/fragments_segnalazioni/vistaSegnalazione.java
@@ -3,6 +3,7 @@ package fragments_segnalazioni;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -13,6 +14,11 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -26,7 +32,7 @@ import it.uniba.dib.sms2223_2.R;
 import model.Segnalazione;
 
 
-public class vistaSegnalazione extends Fragment {
+public class vistaSegnalazione extends Fragment implements OnMapReadyCallback {
 
     private FirebaseAuth auth;
     private FirebaseFirestore db;
@@ -46,8 +52,8 @@ public class vistaSegnalazione extends Fragment {
     private static final String ARG_PARAM1 = "obj";
     private Segnalazione s;
 
-
-
+    private MapView mapView;
+    private static final String MAPVIEW_BUNDLE_KEY="MapViewBundleKey";
 
 
     @Override
@@ -56,6 +62,7 @@ public class vistaSegnalazione extends Fragment {
         if (getArguments() != null) {
             s= (Segnalazione) getArguments().getSerializable(ARG_PARAM1);
         }
+
 
     }
 
@@ -66,7 +73,13 @@ public class vistaSegnalazione extends Fragment {
 
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_vista_segnalazione, container, false);
-
+        Bundle mapViewBundle=null;
+        if(savedInstanceState!=null){
+            mapViewBundle=savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
+        mapView=(MapView) rootView.findViewById(R.id.mapView);
+        mapView.onCreate(mapViewBundle);
+        mapView.getMapAsync(this);//imposta un oggeto di callback che verrà attivato quando l'istanza di google map è pronta per essere utilizzata
 
 
         descrizioneReport=rootView.findViewById(R.id.DescrizioneVistaReport);
@@ -97,7 +110,52 @@ public class vistaSegnalazione extends Fragment {
         return rootView;
     }
 
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Bundle mapViewBundle=outState.getBundle(MAPVIEW_BUNDLE_KEY);
+        if(mapViewBundle==null){
+            mapViewBundle=new Bundle();
+            outState.putBundle(MAPVIEW_BUNDLE_KEY,mapViewBundle);
+        }
+        mapView.onSaveInstanceState(mapViewBundle);
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
 
     public Fragment newInstance(Segnalazione param1) {
         vistaSegnalazione fragment = new vistaSegnalazione();
@@ -108,5 +166,10 @@ public class vistaSegnalazione extends Fragment {
         return fragment;
 
 
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(0,0)).title("Marker"));
     }
 }
