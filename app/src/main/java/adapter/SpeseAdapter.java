d3e03c1f9acbf7ee7f6382128a29fa259034c5d9
martinopagano <104270272+martinocosimopagano@users.noreply.github.com>
2022-12-01 00:11:52 +0100

    implementazione riclerview

---
+++ app/src/main/java/adapter/SpeseAdapter.java
@@ -0,0 +1,85 @@
+package adapter;
+
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+
+import it.uniba.dib.sms2223_2.R;
+import model.SpesaAnimale;
+
+//Creo la classe View AnimalAdapter che contiene i riferimenti ai widget della recycleViewAnimal da popolare
+public class SpeseAdapter extends RecyclerView.Adapter<SpeseAdapter.ViewHolder>{
+    //Array con tutti i dati sugli animali da inserire nella view
+    private ArrayList<SpesaAnimale> localDataSet;
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private  TextView descrizione;
+        private  TextView data;
+
+        public TextView getDescrizione() {
+            return descrizione;
+        }
+
+        public TextView getData() {
+            return data;
+        }
+
+        public ViewHolder(View view) {
+            super(view);
+
+            //Prendo i riferimenti ai widget
+            descrizione = (TextView) view.findViewById(R.id.descrizione);
+            data = (TextView) view.findViewById(R.id.data);
+        }
+    }
+
+    // method for filtering our recyclerview items.
+    public void filterList(ArrayList<SpesaAnimale> filterlist) {
+        // below line is to add our filtered
+        // list in our course array list.
+        localDataSet = filterlist;
+        // below line is to notify our adapter
+        // as change in recycler view data.
+        notifyDataSetChanged();
+    }
+
+    //Funzione richiamata dal fragment myAnimals,il quale passa i dati degli animali
+    public SpeseAdapter(ArrayList<SpesaAnimale> dataSet){
+        localDataSet = dataSet;
+    }
+
+    @NonNull
+    @Override
+    public SpeseAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recycle_view_spese, parent, false);
+
+        return new ViewHolder(v);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return localDataSet.size();
+    }
+}
--- app/src/main/java/fragments/spese.java
+++ app/src/main/java/fragments/spese.java
@@ -2,65 +2,80 @@ package fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+
+import adapter.AnimalAdapter;
+import adapter.SpeseAdapter;
 import it.uniba.dib.sms2223_2.R;
+import model.Animale;
+import model.SpesaAnimale;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link spese#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class spese extends Fragment {
+    private FirebaseAuth auth;
+    private FirebaseFirestore db;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private FloatingActionButton addSpesa;
+    protected RecyclerView mRecyclerView;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    protected ArrayList<SpesaAnimale> mDataset= new ArrayList<>();
+    protected SpeseAdapter mAdapter;
 
-    public spese() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment spese.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static spese newInstance(String param1, String param2) {
-        spese fragment = new spese();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_spese, container, false);
+        addSpesa = rootView.findViewById(R.id.aggiungiSpesaBtn);
+
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recycleSpese);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        // simulo un dataset TODO ------
+        mDataset.add(new SpesaAnimale());
+        mDataset.add(new SpesaAnimale());
+
+        // lo setto -------- TODO-------
+        mAdapter = new SpeseAdapter(mDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
+        //Inizializzo l'ascoltatore al click dell'item
+        mRecyclerView.addOnItemTouchListener(
+                new RecyclerItemClickListener(getActivity().getApplicationContext(), mRecyclerView ,new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override public void onItemClick(View view, int position) {
+                    }
+
+                    @Override public void onLongItemClick(View view, int position) {
+                    }
+                })
+        );
+
+
+        addSpesa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity().getApplicationContext(), "Aggiungi spesa!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return rootView;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_spese, container, false);
+    private void startDatabase(){
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
     }
 }
